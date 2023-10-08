fcd1f9022e05c106983fdb2c488c9f5e185301d7
hellobro <144770256+dxghozali@users.noreply.github.com>
2023-10-08 11:31:05 +0700

    Create for statement.js

---
+++ for statement.js
@@ -0,0 +1,8 @@
+  <script>
+        var ul = document.getElementById("daftar angka nih");
+        for (var i = 10; i <=100; i++) {
+            var li = document.createElement("li");
+            li.textContent =i;
+            ul.appendChild(li);
+        }
+  </script>
